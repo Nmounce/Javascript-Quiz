657ef9d5a7cde17555c44e3c4980f222afb2887d
NikkiMounce <nikkimounce@gmail.com>
2021-07-14 13:17:00 -0700

    made html and started js

---
+++ assets/script/q&a.js
@@ -0,0 +1,91 @@
+// array- passing in number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question:"",
+        answer:"",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        numb: 1,
+        question:"",
+        answer:"",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        numb: 1,
+        question:"",
+        answer:"",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        numb: 1,
+        question:"",
+        answer:"",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        numb: 1,
+        question:"",
+        answer:"",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        numb: 1,
+        question:"",
+        answer:"",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        numb: 1,
+        question:"",
+        answer:"",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        numb: 1,
+        question:"",
+        answer:"",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    },  
+];
